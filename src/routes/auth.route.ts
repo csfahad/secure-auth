4a5c3bf6395cb7c967b4485bd631891cc924f4cf
csfahad <dev.csfahad@gmail.com>
2025-10-15 20:22:50 +0530

    feat(route): defined verify-otp route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/auth.controller";
+import {
+    login,
+    logout,
+    register,
+    verifyOtpHandler,
+} from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/verify-otp", verifyOtpHandler);
 router.post("/login", login);
 router.post("/logout", logout);
 
