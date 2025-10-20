5a895816241acc09770c06fcbd133f0c46b38053
csfahad <dev.csfahad@gmail.com>
2025-10-20 16:17:13 +0530

    feat(zod-schema): defined change-password zod schema

--- src/validators/authSchema.ts
+++ src/validators/authSchema.ts
@@ -96,3 +96,17 @@ export const resetPasswordSchema = z.object({
     token: z.string(),
     newPassword: z.string().min(8, "Password must be atleast 8 characters"),
 });
+
+export const changePasswordSchema = z.object({
+    currentPassword: z.string().min(8, "Current password is required"),
+    newPassword: z
+        .string()
+        .min(8, "Password must be at least 8 characters long")
+        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        .regex(/\d/, "Password must contain at least one number")
+        .regex(/[!@#$%^&*?]/, {
+            message:
+                "Password must contain at least one special character (!@#$%^&*?)",
+        }),
+});
