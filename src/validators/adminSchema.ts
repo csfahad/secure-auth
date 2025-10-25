1799878fcf2544d8e2484a5c4fb10dbcfed19a53
csfahad <dev.csfahad@gmail.com>
2025-10-25 19:53:04 +0530

    feat(zod-schema): defined zod-validation for get-all-users schema

--- src/validators/adminSchema.ts
+++ src/validators/adminSchema.ts
@@ -1,5 +1,24 @@
 import { z } from "zod";
 
+export const getAllUsersSchema = z.object({
+    page: z
+        .string()
+        .optional()
+        .transform((val) => (val ? parseInt(val, 10) : 1))
+        .refine((val) => val > 0, "Page must be greater than 0"),
+
+    limit: z
+        .string()
+        .optional()
+        .transform((val) => (val ? parseInt(val, 10) : 10))
+        .refine(
+            (val) => val > 0 && val <= 100,
+            "Limit must be between 1 and 100"
+        ),
+
+    search: z.string().optional(),
+});
+
 export const updateUserRoleSchema = z.object({
     userId: z.uuid("Invalid user Id"),
     role: z
