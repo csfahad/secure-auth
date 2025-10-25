ee4ffc6cc31edbd8c45bd664de566c5c9f30d220
csfahad <dev.csfahad@gmail.com>
2025-10-25 19:06:06 +0530

    feat(controller): implemented fetch-users and update-user-role in admin-controller

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,71 @@
+import { Response } from "express";
+import prisma from "../lib/prisma";
+import { AuthenticatedRequest } from "../types/express";
+import { updateUserRoleSchema } from "../validators/adminSchema";
+import { z } from "zod";
+
+// fetch all users (Admin and SuperAdmin only)
+export const getAllUsers = async (res: Response) => {
+    try {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                phone: true,
+                role: true,
+                createdAt: true,
+            },
+            orderBy: { createdAt: "desc" },
+        });
+
+        return res.status(200).json({ "Total users": users.length, users });
+    } catch (err) {
+        console.error("AdminController -> getAllUsers error:", err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+// Update user role (SuperAdmin-only)
+export const updateUserRole = async (
+    req: AuthenticatedRequest,
+    res: Response
+) => {
+    const parsed = updateUserRoleSchema.safeParse(req.body);
+    if (!parsed.success) {
+        return res.status(400).json({ error: z.treeifyError(parsed.error) });
+    }
+
+    const { userId, role } = parsed.data;
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: userId },
+            data: { role },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                phone: true,
+                role: true,
+            },
+        });
+
+        return res.status(200).json({
+            message: `User role updated to ${role}`,
+            user: updatedUser,
+        });
+    } catch (err) {
+        console.error("AdminController -> updateUserRole error:", err);
+
+        if (err instanceof z.ZodError) {
+            return res.status(400).json({ error: err.message });
+        }
+
+        if ((err as any).code === "P2025") {
+            // prisma: record not found
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        return res.status(500).json({ error: "Internal server error" });
+    }
+};
