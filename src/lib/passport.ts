3ca72b5f7d169f523e4fc9dfb05a3364402a1216
csfahad <dev.csfahad@gmail.com>
2025-10-27 17:29:26 +0530

    fix(OAuth): fixed minute typo

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -1,11 +1,11 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy, Strategy } from "passport-google-oauth20";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import prisma from "./prisma";
 import { createSession } from "../services/sessionService";
 import { generateAccessToken } from "../utils/jwt";
 
 passport.use(
-    new Strategy(
+    new GoogleStrategy(
         {
             clientID: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
